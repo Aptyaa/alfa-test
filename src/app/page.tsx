f1c6638f8892e74cd1916efa8b6e3739127d387f
Denis Shulga <74553411+Aptyaa@users.noreply.github.com>
2025-07-28 20:56:30 +0400

    Update page.tsx
    add tg widget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,33 @@
+'use client'
+import { useEffect, useRef } from 'react'
 import CustomLink from './ui/product/custom-link'
+	declare global {
+		interface Window {
+			onTelegramAuth: (user: any) => void
+		}
+	}
 
-export default async function Home() {
-	const response = await fetch("https://api.telegram.org/bot7605252858:AAFAaO2qnApQYmMkEFI7tMvWQJmySUrHn5M/getMe")
-	const data = await response.json()
+export default function Home() {
+	
+	const onTelegramAuth = (user:any) : void=> {
+    alert('Logged in as ' + user.first_name + ' ' + user.last_name + ' (' + user.id + (user.username ? ', @' + user.username : '') + ')');
+  }
+
+	useEffect(() => {
+		window.onTelegramAuth = onTelegramAuth
+
+		const script = document.createElement('script')
+		script.src = 'https://telegram.org/js/telegram-widget.js?22'
+		script.async = true
+		script.setAttribute('data-telegram-login', 'aDatingService_bot')
+		script.setAttribute('data-size', 'large')
+		script.setAttribute('data-userpic', 'false')
+		script.setAttribute('data-onauth', 'onTelegramAuth(user)')
+		script.setAttribute('data-request-access', 'write')
+
+		document.body.appendChild(script)
+	}, [])
+
 	return (
 		<>
 			<div className='bg-white flex mx-2 items-center justify-center min-h-120 rounded-xl shadow-2xl p-8 max-w-2xl w-full text-center'>
@@ -10,10 +35,7 @@ export default async function Home() {
 					<h1 className='text-2xl sm:text-5xl font-bold text-gray-900 mb-4'>
 						Добро пожаловать на барахолку!
 					</h1>
-					<div>{JSON.stringify(data)}</div>
-					<h3 className='text-l sm:text-2xl text-gray-700 mb-8'>
-						Здесь вы найдете самые разные вещи!!!
-					</h3>
+					<h3 className='text-l sm:text-2xl text-gray-700 mb-8'>Здесь вы найдете самые разные вещи!!!</h3>
 					<CustomLink size='large' href='/products'>
 						Смотреть
 					</CustomLink>
